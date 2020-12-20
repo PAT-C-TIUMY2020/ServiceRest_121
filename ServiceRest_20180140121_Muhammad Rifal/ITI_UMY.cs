35a7786168606defcfff2da7506441b9182440ce
MuhammadRifal22 <rifalmuhammad797@gmail.com>
2020-12-20 10:21:31 +0700

    Add project files.

---
+++ ServiceRest_20180140121_Muhammad Rifal/ITI_UMY.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.ServiceModel.Web;
+using System.Text;
+
+namespace ServiceRest_20180140121_Muhammad_Rifal
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the interface name "IService1" in both code and config file together.
+    [ServiceContract]
+    public interface ITI_UMY
+    {
+        [OperationContract]
+        [WebGet(UriTemplate = "Mahasiswa", ResponseFormat = WebMessageFormat.Json)] //untuk membuat slash, selalu realtive
+        List<Mahasiswa> GetAllMahasiswa(); //mendapatkan kumpulan mahasiswa/seluruh data mahasiswa
+
+        [OperationContract]                          //nama method
+        [WebGet(UriTemplate = "Mahasiswa/nim={nim}", ResponseFormat = WebMessageFormat.Json)] //untuk get
+        Mahasiswa GetMahasiswaByNIM(string nim); //mengambil data berdasarkan nim
+
+        //void CreateMahasiswa(mahasiswa mhs); tidak ada pengembalian/respond balik
+        [OperationContract]
+        [WebInvoke(Method = "POST", UriTemplate = "Mahasiswa", RequestFormat = WebMessageFormat.Json, ResponseFormat = WebMessageFormat.Json)]
+        string CreateMahasiswa(Mahasiswa mhs);
+
+        // TODO: Add your service operations here
+    }
+
+    // Use a data contract as illustrated in the sample below to add composite types to service operations.
+    // You can add XSD files into the project. After building the project, you can directly use the data types defined there, with the namespace "ServiceRest_20180140093_Musafak.ContractType".
+    [DataContract]
+    public class Mahasiswa
+    {
+        private string _nama, _nim, _prodi, _angkatan; //adalah konvensi atau kesepakatan variable lokal
+        [DataMember(Order = 1)] //mengirim data untuk mengurutkan
+        public string nama
+        {
+            get { return _nama; }
+            set { _nama = value; }
+        }
+        [DataMember(Order = 2)]
+        public string nim
+        {
+            get { return _nim; }
+            set { _nim = value; }
+        }
+        [DataMember(Order = 3)]
+        public string prodi
+        {
+            get { return _prodi; }
+            set { _prodi = value; }
+        }
+        [DataMember(Order = 4)]
+        public string angkatan
+        {
+            get { return _angkatan; }
+            set { _angkatan = value; }
+        }
+    }
+}
---
+++ ServiceRest_20180140121_Muhammad Rifal/TI_UMY.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace ServiceRest_20180140121_Muhammad_Rifal
+{
+    // NOTE: You can use the "Rename" command on the "Refactor" menu to change the class name "Service1" in both code and config file together.
+    public class TI_UMY : ITI_UMY
+    {
+        public string CreateMahasiswa(Mahasiswa mhs)
+        {
+            string msg = "GAGAL";
+            SqlConnection sqlcon = new SqlConnection("Data Source=LAPTOP-8FSMF5N0;Initial Catalog=TI UMY;Persist Security Info=True;User ID=sa; Password=12345678");
+            string query = String.Format("insert into dbo.Mahasiswa values ('{0}','{1}','{2}','{3}')", mhs.nama, mhs.nim, mhs.prodi, mhs.angkatan);
+            SqlCommand sqlcom = new SqlCommand(query, sqlcon);
+            try
+            {
+                sqlcon.Open();
+                Console.WriteLine(query);
+                sqlcom.ExecuteNonQuery();
+                sqlcon.Close();
+                msg = "Sukses";
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                Console.WriteLine(query);
+                msg = "GAGAL";
+            }
+
+
+            return msg;
+        }
+
+        public List<Mahasiswa> GetAllMahasiswa()
+        {
+            {
+                List<Mahasiswa> mahas = new List<Mahasiswa>();
+
+                SqlConnection con = new SqlConnection("Data Source=DESKTOP-5L26KM2;Initial Catalog=TI UMY;Persist Security Info=True;User ID=sa; Password=musafak93");
+                string query = "select Nama, NIM, Prodi, Angkatan from dbo.Mahasiswa";
+                SqlCommand com = new SqlCommand(query, con); //yang dikirim ke sql
+
+                try
+                {
+                    con.Open();
+                    SqlDataReader reader = com.ExecuteReader(); //mendapatkan data yang telah dieksekusi, dari select, hasil query ditaro direader
+
+                    while (reader.Read())
+                    {
+                        Mahasiswa mhs = new Mahasiswa();
+                        mhs.nama = reader.GetString(0); //0 itu array pertama diambil dari iservice
+                        mhs.nim = reader.GetString(1);
+                        mhs.prodi = reader.GetString(2);
+                        mhs.angkatan = reader.GetString(3);
+
+                        mahas.Add(mhs);
+                    }
+                    con.Close();
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                    Console.WriteLine(query);
+                }
+                return mahas;
+            }
+        }
+
+        public Mahasiswa GetMahasiswaByNIM(string nim)
+        {
+            Mahasiswa mhs = new Mahasiswa();
+            SqlConnection con = new SqlConnection("Data Source=DESKTOP-5L26KM2;Initial Catalog=TI UMY;Persist Security Info=True;User ID=sa; Password=musafak93");
+            string query = String.Format("select Nama, NIM, Prodi, Angkatan from dbo.Mahasiswa where NIM = '{0}'", nim);
+            SqlCommand com = new SqlCommand(query, con);
+
+            try
+            {
+                con.Open();
+                SqlDataReader reader = com.ExecuteReader();
+
+                while (reader.Read())
+                {
+                    mhs.nama = reader.GetString(0); //0 array pertama diambil dari iservice
+                    mhs.nim = reader.GetString(1);
+                    mhs.prodi = reader.GetString(2);
+                    mhs.angkatan = reader.GetString(3);
+                }
+                con.Close();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                Console.WriteLine(query);
+            }
+            return mhs;
+        }
+    }
+}
